0893b315ec9ab4b93231f95fb520091ad8a793f7
Badbatunde <hb.oyesile@gmail.com>
2023-02-02 19:25:24 +0100

    Added some fundamental tutorial and code snippets.

---
+++ script.js
@@ -0,0 +1,18 @@
+// console.log("1" + 1)
+
+// You can easily increment or add one to a variable with the ++ operator.
+
+// i++;
+// is the equivalent of
+
+// i = i + 1;
+
+// In order to get the last letter of a string, you can subtract one from the string's length.
+
+// For example, if const firstName = "Ada", you can get the value of the last letter of the string by using firstName[firstName.length - 1].
+
+// Example:
+
+// const firstName = "Ada";
+// const lastLetter = firstName[firstName.length - 1];
+// lastLetter would have a value of the string a.
