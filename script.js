238e40e17224b3a009ef44d98a2e6ebd597e5685
Badbatunde <hb.oyesile@gmail.com>
2023-02-03 01:13:12 +0100

    Added some new snippets

--- script.js
+++ script.js
@@ -16,3 +16,19 @@
 // const firstName = "Ada";
 // const lastLetter = firstName[firstName.length - 1];
 // lastLetter would have a value of the string a.
+
+// Shopping List
+// Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+// The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+// ["Chocolate Bar", 15]
+// There should be at least 5 sub-arrays in the list.
+
+const myList = [
+    ['Bread', 20],
+    ['Monster Drink', 25],
+    ['Choc bar', 15],
+    ['Jacket', 5],
+    ['Trousers', 50]
+  ];
