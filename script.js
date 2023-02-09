91bf1d013478ce42129a0ded95b27765ab482809
Badbatunde <hb.oyesile@gmail.com>
2023-02-10 00:24:07 +0100

    Made a countdown function

--- script.js
+++ script.js
@@ -235,3 +235,18 @@ console.log(checkSign(10))
 console.log(checkSign(-2))
 console.log(checkSign(2))
 console.log(checkSign(0))
+
+// Countdown function
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+
+console.log(countdown(-1))
+console.log(countdown(10))
+console.log(countdown(5))
