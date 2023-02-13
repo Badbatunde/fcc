3be818f65746ef868a8d97dfa943f28d6508c7cd
Badbatunde <hb.oyesile@gmail.com>
2023-02-13 18:11:23 +0100

    Used Destructuring Assignment to Extract Values from Objects

--- script.js
+++ script.js
@@ -280,3 +280,13 @@ function freezeObj() {
   return MATH_CONSTANTS.PI;
 }
 const PI = freezeObj();
+
+// Use Destructuring Assignment to Extract Values from Objects
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
