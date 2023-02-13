84cf00252b6666f3a3bee273671d0c90a279b46c
Badbatunde <hb.oyesile@gmail.com>
2023-02-13 04:03:40 +0100

    Added a new function about preventing object mutation

--- script.js
+++ script.js
@@ -265,4 +265,21 @@ function rangeOfNumbers(startNum, endNum) {
 
 console.log(rangeOfNumbers(2, 9));
 
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
  
