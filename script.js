5f4eb6da77e049a30496a25ca5c6cd9f7c37b0d0
Badbatunde <hb.oyesile@gmail.com>
2023-02-15 04:36:12 +0100

    Use class Syntax to Define a Constructor Function

--- script.js
+++ script.js
@@ -325,3 +325,13 @@ function makeList(arr) {
 
 const failuresList = makeList(result.failure);
 console.log(failuresList)
+
+// Use class Syntax to Define a Constructor Function
+class Vegetable {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
