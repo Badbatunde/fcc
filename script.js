f99c1ebeafff137ffcf7dd50a0008fb23a0679ca
Badbatunde <hb.oyesile@gmail.com>
2023-02-28 19:33:43 +0100

    modified the function

--- script.js
+++ script.js
@@ -502,5 +502,11 @@ function Dog(name) {
 
 Dog.prototype = {
   // Only change code below this line
-
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
 };
