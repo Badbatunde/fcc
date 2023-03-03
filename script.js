1b29dd5dd0a23c1538b382f9ece2f94fe71e67a5
Badbatunde <hb.oyesile@gmail.com>
2023-03-03 04:57:29 +0100

    Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem

--- script.js
+++ script.js
@@ -517,3 +517,13 @@ let fixedValue = 4;
 function incrementer(increase) {
   return increase = fixedValue + 1;
 }
+
+// Using Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+const squareList = arr => {
+  return arr
+  .filter(num => num > 0 && num % parseInt(num) === 0)
+  .map(num => Math.pow(num, 2))
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
