17abfec0332e364825cb156de73c62a3622b06a7
Badbatunde <hb.oyesile@gmail.com>
2023-03-06 09:06:24 +0100

    DNA Pairing result

--- script.js
+++ script.js
@@ -595,7 +595,27 @@ myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
 
 // DNA Pairing
 function pairElement(str) {
-  return str;
+  // Function to match each character with the base pair
+  const matchWithBasePair = function(char) {
+    switch (char) {
+      case "A":
+        return ["A", "T"];
+      case "T":
+        return ["T", "A"];
+      case "C":
+        return ["C", "G"];
+      case "G":
+        return ["G", "C"];
+    }
+  };
+
+  // Find pair for every character in the string
+  const pairs = [];
+  for (let i = 0; i < str.length; i++) {
+    pairs.push(matchWithBasePair(str[i]));
+  }
+
+  return pairs;
 }
 
-pairElement("GCG");
+pairElement("GCG");                           
